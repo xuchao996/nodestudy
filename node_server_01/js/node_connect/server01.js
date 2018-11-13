db1175b53b884ab552c75b2f3e2ba8d2550b8524
daXiaxuchao <1072998149@qq.com>
2018-11-13 23:39:09 +0800

    增加cookie test

--- node_server_01/js/node_connect/server01.js
+++ node_server_01/js/node_connect/server01.js
@@ -4,11 +4,16 @@ const connect = require('connect');
 var bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+// cookieParser()
+/**
+ * @param 私钥，
+ */
 var app = connect()
-	.use(cookieParser('tobi afasdf'))
+	.use(cookieParser())
 	.use(function (req, res) {
 		console.log(req.cookies);
-		console.log(req.signCookies);
+		console.log(req.signedCookies);
+		res.setHeader('Cookie', 'text/html')
 		res.setHeader('Content-Type', 'text/html')
 		res.end('hello\n');
 	})
